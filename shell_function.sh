d8ad0164a81a7e9f90ce17cdbaa8bb48c26f0ef4
Aditya Gupta <102688268+Aditya-1998k@users.noreply.github.com>
2024-05-31 13:15:01 +0530

    Update shell_function.sh

--- shell_function.sh
+++ shell_function.sh
@@ -10,3 +10,13 @@ it has been running since ${since}
 EOF
 }
 showuptime
+
+
+<<comments
+gaditya@lptl-gaditya:~/instances$ chmod u+x shell_function.sh
+gaditya@lptl-gaditya:~/instances$ ./shell_function.sh 
+------------------
+This machine has been up for 2 hours, 7 minutes
+it has been running since 2024-05-31 11:06:11
+-------------------
+comments
